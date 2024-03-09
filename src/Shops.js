0676e0464aab23efe4ca2431c308321d501bec12
Sofiia Maliarenko <maliarenko.sofiia@gmail.com>
2024-03-09 17:02:07 +0200

    Update visuals

--- src/Shops.js
+++ src/Shops.js
@@ -22,22 +22,26 @@ const Shops = () => {
     <Container>
       <ShopNavigation />
       <div>
-        <Row className="mt-3">
-          <Col md={4} className="pt-3">
+        <Row className="mt-3 d-flex justify-content-between">
+          <Col md={3} className="shops pt-3">
             <p>
               <b>Choose a Drug store:</b>
             </p>
             {renderShopButtons()}
           </Col>
-          <Col md={4}>
-            <MedicineCards data={"Paracetamol"} />
-            <MedicineCards data={"Paracetamol"} />
-            <MedicineCards data={"Paracetamol"} />
-          </Col>
-          <Col md={4}>
-            <MedicineCards data={"Paracetamol"} />
-            <MedicineCards data={"Paracetamol"} />
-            <MedicineCards data={"Paracetamol"} />
+          <Col md={9}>
+            <Row className="d-flex justify-content-end">
+              <Col md={5}>
+                <MedicineCards data={"Paracetamol"} />
+                <MedicineCards data={"Paracetamol"} />
+                <MedicineCards data={"Paracetamol"} />
+              </Col>
+              <Col md={4}>
+                <MedicineCards data={"Paracetamol"} />
+                <MedicineCards data={"Paracetamol"} />
+                <MedicineCards data={"Paracetamol"} />
+              </Col>
+            </Row>
           </Col>
         </Row>
       </div>
